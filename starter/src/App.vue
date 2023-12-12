b03692de3c3128a0a978f5d0d947fe70cafeba1e
정재원 <popt0@naver.com>
2023-12-12 18:35:08 +0900

    install

---
+++ starter/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <vue-header />
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import vueHeader from './components/layout/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    vueHeader
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ starter/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Welcome to {{inputData}}!!</h1>
+        <input type="text" v-model="inputData" />
+        <button @click=getData>붯튼</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "theHome",
+    data() {
+        return {
+            title: "가보즈아아아아!!",
+            inputData: "",
+        }
+    },
+    methods: {
+        getData() {
+            alert(this.inputData);
+        }
+    }
+}
+</script>
