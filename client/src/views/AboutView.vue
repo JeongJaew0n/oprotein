444b4d9f8df95d79f2dcc2766fa050508458f5f0
HeLLo2 <popt0@naver.com>
2024-01-18 21:42:40 +0900

    chore: setting

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Button @click="count++">Click me</Button>
+    <span>{{ count }}</span>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script lang="ts">
+import { Button } from '@/components/ui/button'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+
+    return {
+      count
+    }
+  }
+}
+
+</script>
