35eeae7ab89d5a5f1c2db464d9c886a3dc48299c
JeongJaew0n <popt0@naver.com>
2025-01-22 23:48:25 +0900

    fix: can't resolve error 해결
    - web-vitals 라이브러리 다운로드 실패. 그냥 파일명 바꿈
    - index.tsx 에서 App.tsx import 실패해서 .tsx 명시

---
+++ src/example/reactHookFormExample.tsx
@@ -0,0 +1,76 @@
+import {
+    SubmitErrorHandler,
+    SubmitHandler,
+    useForm,
+  } from 'react-hook-form';
+  
+  /** 폼 데이터 정의 */
+  interface FormData {
+    name: string;
+    email: string;
+    newPassword: string;
+    retypePassword: string;
+    nation: string;
+    acceptTos: boolean;
+  }
+  
+  /** 폼 컴포넌트 */
+  const FormExample = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+  
+    // 제출 성공 이벤트 핸들러 함수
+    const handleSuccess: SubmitHandler<FormData> = (data) => {
+      console.log('입력 데이터', data)
+    };
+  
+    // 제출 실패 이벤트 핸들러 함수
+    const handleError: SubmitErrorHandler<FormData> = (error) => {
+      console.log('에러 데이터', error)
+    };
+  
+    return (
+      <form className="form-container" onSubmit={handleSubmit(handleSuccess, handleError)}>
+        <h2>회원가입</h2>
+        <div className="input-container">
+          <label htmlFor="name">이름: </label>
+          <input type="text" {...register('name')} />
+        </div>
+  
+        <div className="input-container">
+          <label htmlFor="nation">국적: </label>
+          <select {...register('nation')}>
+            <option value="ko">대한민국</option>
+            <option value="zh">중국</option>
+            <option value="ja">일본</option>
+          </select>
+        </div>
+  
+        <div className="input-container">
+          <label htmlFor="email">이메일: </label>
+          <input type="email" {...register('email')} />
+        </div>
+  
+        <div className="input-container">
+          <label htmlFor="email">패스워드: </label>
+          <input type="password" {...register('newPassword')} />
+        </div>
+  
+        <div className="input-container">
+          <label htmlFor="email">패스워드 재입력: </label>
+          <input type="password" {...register('retypePassword')} />
+        </div>
+  
+        <div className="input-container">
+          <label htmlFor="email">서비스 이용약관에 동의합니다 </label>
+          <input type="checkbox" {...register('acceptTos')} />
+        </div>
+  
+        <div className="button-container column">
+          <button type="submit">회원가입</button>
+        </div>
+      </form>
+    );
+  };
+  
+  export default FormExample;
+  
