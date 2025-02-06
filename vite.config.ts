5cdc7dd657423bfa02d593a8dcd64ad82c7deaf1
정재원/기술지원팀/II <popt0@injeinc.co.kr>
2025-02-06 11:25:40 +0900

    chore: tsconfig 추가 및 설정
    - vite.config.ts대신 tsconfig의 경로설정을 적용하려면 vite-tsconfig-paths 라이브러리 필요함!! package.json에 다른 파일이 많이 추가되어 현 commit 에서는 제외

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode, command }) => {
 
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     server: {
       port: 3700
-    }
+    },
   }
 });
